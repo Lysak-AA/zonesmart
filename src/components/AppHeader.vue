7cee55b930ae1fd1fe725cbb6046ab292292b327
Lysak-AA <frigh701@gmail.com>
2024-05-03 11:05:54 +1000

    add app header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  header(class='app-header')
+    <AppLogo />
+</template>
+
+
+<script>
+import AppLogo from '@/components/icons/AppLogo.vue'
+
+export default {
+  name: 'AppHeader',
+  components: {
+    AppLogo
+  }
+}
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,15 @@
-<template>
-  <div class="login">
-  </div>
+<template lang="pug">
+.login-page
+  <AppHeader />
 </template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+
+export default {
+  name: 'login',
+  components: {
+    AppHeader
+  }  
+}
+</script>
