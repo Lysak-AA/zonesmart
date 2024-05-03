6244a7d60e11726c50be737fc866a5e37c5efa39
Lysak-AA <frigh701@gmail.com>
2024-05-03 12:39:59 +1000

    create input component

---
+++ src/components/AppInput.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+.app-input
+  label(class="app-input-label", :for="id") {{ inputLabel }}
+  input(class="app-input-field" :type="inputType", :id="id", :placeholder="inputPlaceholder")
+</template>
+
+<script>
+export default {
+  name: 'AppInput',
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    inputLabel: {
+      type: String,
+      default: ''
+    },
+    inputType: {
+      type: String,
+      default: 'text'
+    },
+    inputPlaceholder: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
--- src/components/AppLoginForm.vue
+++ src/components/AppLoginForm.vue
@@ -1,3 +1,18 @@
 <template lang="pug">
 .app-login-form
+  h1(class='app-login-form__title') Вход
+  span(class='app-login-form__registration-cta') Нет аккаунта? 
+    a(class='app-login-form__registration-cta-link', href="#") Зарегистрироваться
+  <AppInput id="email" inputLabel="Email" inputType="email" inputPlaceholder="youremail@mail.ru" />
 </template>
+
+<script>
+import AppInput from '@/components/AppInput.vue'
+
+export default {
+  name: 'AppLoginForm',
+  components: {
+    AppInput
+  }
+}
+</script>
