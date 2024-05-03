83111cfb15313b4f9d86a509fa67db0c5bbd8a88
Lysak-AA <frigh701@gmail.com>
2024-05-03 12:08:30 +1000

    login form layout

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
 .login-page
   <AppHeader />
+  main(class='login-page-main')
+    <AppLoginForm />
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue'
+import AppLoginForm from '@/components/AppLoginForm.vue'
 
 export default {
   name: 'login',
   components: {
-    AppHeader
+    AppHeader,
+    AppLoginForm
   }  
 }
 </script>
