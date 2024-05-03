462ce4c5beb16fe63b38654e1147c1e32f5334fc
Lysak-AA <frigh701@gmail.com>
2024-05-03 11:30:15 +1000

    add fonts

---
+++ src/components/AppContactsPanel.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+  .app-contacts-panel
+    a(class='app-contacts-panel__support-link', href="#") Поддержка
+</template>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
   header(class='app-header')
     <AppLogo />
+    <AppContactsPanel />
 </template>
 
 
 <script>
 import AppLogo from '@/components/icons/AppLogo.vue'
+import AppContactsPanel from '@/components/AppContactsPanel.vue'
 
 export default {
   name: 'AppHeader',
   components: {
-    AppLogo
+    AppLogo,
+    AppContactsPanel
   }
 }
 </script>
