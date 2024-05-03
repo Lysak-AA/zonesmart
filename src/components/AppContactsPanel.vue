d34e4e9e5c2e7042bb107c540b113bfc03963c03
Lysak-AA <frigh701@gmail.com>
2024-05-03 11:36:08 +1000

    add icons components

--- src/components/AppContactsPanel.vue
+++ src/components/AppContactsPanel.vue
@@ -1,4 +1,18 @@
 <template lang="pug">
   .app-contacts-panel
     a(class='app-contacts-panel__support-link', href="#") Поддержка
+    <IconTelegram />
+    <IconWhatsapp />
 </template>
+
+<script>
+import IconTelegram from '@/components/icons/IconTelegram.vue'
+import IconWhatsapp from '@/components/icons/IconWhatsapp.vue'
+export default {
+  name: 'AppContactsPanel',
+  components: {
+    IconTelegram,
+    IconWhatsapp
+  }
+}
+</script>
