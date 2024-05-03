3e40f37ae82130c25b5cde073112ff911310fada
Lysak-AA <frigh701@gmail.com>
2024-05-03 10:52:40 +1000

    add vue pug plugin

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import vuePugPlugin from 'vue-pug-plugin'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,7 +7,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        preprocessOptions: {
+          plugins: [
+            vuePugPlugin
+          ]
+        }
+      }
+    }),
   ],
   resolve: {
     alias: {
